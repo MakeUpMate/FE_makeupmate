1291afc578fa1f207bf1dd6a4b701f0fb93dd1f6
tinne14 <97443125+tinne14@users.noreply.github.com>
2023-06-03 10:58:04 +0700

    implement xml, logic camera, login signup with firebase

--- app/src/main/java/com/example/makeupmate/MainActivity.kt
+++ app/src/main/java/com/example/makeupmate/MainActivity.kt
@@ -4,15 +4,29 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.makeupmate.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.btnLogout.setOnClickListener {
+            firebaseAuth.signOut()
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            intent.flags =
+                Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+        }
         binding.btnAnalyze.setOnClickListener {
             startActivity(Intent(this, CameraActivity::class.java))
         }
