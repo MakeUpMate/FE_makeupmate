9af5da18c151ce8a8eb4bf1c8249729797c47bee
Nugraha Akt <nugraha.aktwovhiday@ui.ac.id>
2023-06-11 20:50:22 +0700

    add icon

--- app/src/main/java/com/example/makeupmate/ui/SplashScreen.kt
+++ app/src/main/java/com/example/makeupmate/ui/SplashScreen.kt
@@ -26,13 +26,13 @@ class SplashScreen : AppCompatActivity() {
                 val intent = Intent(this@SplashScreen, MainActivity::class.java)
                 intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
                 startActivity(intent)
-            }, 3000)
+            }, 1500)
         } else{
             Handler(Looper.getMainLooper()).postDelayed({
                 val intent = Intent(this@SplashScreen, WelcomeActivity::class.java)
                 intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
                 startActivity(intent)
-            }, 3000)
+            }, 1500)
         }
     }
 }
